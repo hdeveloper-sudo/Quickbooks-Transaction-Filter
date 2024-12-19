efe789bf9e4efc2ef7cae1a922760b31bc431dcb
Hugo <hugoyu2001@gmail.com>
2024-12-19 16:43:13 +1100

    Update transaction_filters.js
    Now uses function-call and true/false function to easily switch between whitelist and blacklist

--- transaction_filters.js
+++ transaction_filters.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide transactions from Quickbook
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-18
+// @version      2024-12-19
 // @description  Great for businesses who have multiple accountants working on different kinds of transactions
 // @author       You
 // @match        https://qbo.intuit.com/app/banking
@@ -13,52 +13,34 @@
     'use strict';
 
     // Your code here...
+    // List of filter words. Feel free to change
+    const filterWords = [
+        "deposit",
+        "withdrawal",
+        "refund",
+        "overdraw",
+        "interest",
+        "westpac"
+    ];
+
+    // Configure whether to use filterWords as Blacklist or Whitelist
+    const blacklist = true; // change to false to use filterWords as whitelist instead
+
     setInterval(function() {
         if(document.getElementsByClassName('idsTable__columnGroup')[1] !== undefined){
-            // List of filter words. Feel free to change
-            const filterWords = [
-                "deposit",
-                "withdrawal",
-                "refund",
-                "overdraw",
-                "interest",
-                "westpac"
-            ];
-
             // Class name of transaction table, subject to change by Quickbooks
             var table = document.getElementsByClassName('idsTable__columnGroup')[1];
             // Loop through each row
             for (let r = table.rows.length - 1; r >= 0; r--) {
                 // Check if the row is a data (and not a month heading), as only row heading have the class name 'group-header-row'
                 if (!table.rows[r].classList.contains('group-header-row')) {
-                    // Use Blacklist
-                    //
-                    // Loop through each filter word
-                    filterWords.forEach(filterWord => {
-                        // Remove entire row if 2nd column (being the description) of each row contains one of the filter words
-                        if (table.rows[r].cells[2].innerHTML.toLowerCase().indexOf(filterWord) !== -1) {
-                            table.deleteRow(r);
-                        }
-
-                    })
-                    //
-
-                    // Use Whitelist
-                    /*
-                    // Initiate remove to true, to tell program to delete the row by default
-                    let remove = true;
-                    filterWords.forEach(filterWord => {
-                        // Check if the filterWord in the current iteration is in this row
-                        if (table.rows[r].cells[2].innerHTML.toLowerCase().indexOf(filterWord) !== -1) {
-                            // If the filterWord exists, set deleterow to false so the program does NOT delete the row
-                            remove = false;
-                        }
-                    })
-                    // If deleterow remains true after going through all filterWords, delete the row
-                    if (remove) {
-                        table.deleteRow(r);
+                    if (blacklist) {
+                        // Use Blacklist
+                        Blacklist(table, r);
+                    } else {
+                        // Use Whitelist
+                        Whitelist(table, r);
                     }
-                    */
                 }
             }
 
@@ -88,4 +70,34 @@
             }
         }
 }, 1000);
+
+    // Instructions to Blacklist filterWords using selected table and current iteration
+    function Blacklist(table, r) {
+        // Loop through each filterWord
+        filterWords.forEach(filterWord => {
+            // Remove entire row if 2nd column (being the description) of each row contains one of the filter words
+            if (table.rows[r].cells[2].innerHTML.toLowerCase().indexOf(filterWord) !== -1) {
+                table.deleteRow(r);
+            }
+
+        })
+    }
+
+    // Instructions to Whitelist filterWords using selected table and current iteration
+    function Whitelist(table, r) {
+        // Initiate remove to true, to tell program to delete the row by default
+        let remove = true;
+        // Loop through each filterWord
+        filterWords.forEach(filterWord => {
+            // Check if the filterWord in the current iteration is in this row
+            if (table.rows[r].cells[2].innerHTML.toLowerCase().indexOf(filterWord) !== -1) {
+                // If the filterWord exists, set deleterow to false so the program does NOT delete the row
+                remove = false;
+            }
+        })
+        // If deleterow remains true after going through all filterWords, delete the row
+        if (remove) {
+            table.deleteRow(r);
+        }
+    }
 })();
